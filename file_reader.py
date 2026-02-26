ec39afe640f997e83c26a428cf5ce75182db9bbe
PUDELEK <prometey202020@gmail.com>
2026-02-26 15:05:08 +0100

    Getting PDB structure

--- file_reader.py
+++ file_reader.py
@@ -1,1 +1,11 @@
+from Bio.PDB.PDBParser import PDBParser
+
+"""Getting the PDB structure"""
+par = PDBParser(PERMISSIVE=1)
+structure_id = '1KY9'
+filename = structure_id + '.pdb'
+
+structure = par.get_structure(structure_id, filename)
+
+
 
